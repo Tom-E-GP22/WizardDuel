3c8ac32829e592a2ef4661ce4c62309b463a0cce
Tom <tom.eckerstrom@hotmail.com>
2023-01-16 11:57:18 +0100

    ui bobbing

--- Assets/ButtonHandler.cs
+++ Assets/ButtonHandler.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void AddGameModeSelect()
+    { 
+        //TODO
+        //Add some way for buttons to recognize what gamemode was selected
+        //This method is currently on Host, Join and SinglePlayer buttons
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SelectedCharacter()
     {
-        
+        //TODO
+        //Add way for game to recognize what character was selected
+        //perhaps ENUM?
+    }
+
+    public void ToggleUIElement(GameObject gameObject)
+    {
+        gameObject.SetActive(gameObject.activeInHierarchy);
+    }
+
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
